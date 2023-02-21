05b602708a31d052ac163c949c6fb5e57b9994bf
Ry2uko <gabrielfradz@hotmail.com>
2023-02-22 07:19:29 +0800

    feat: draw hero and shadow

--- script/Overworld.js
+++ script/Overworld.js
@@ -6,12 +6,49 @@ class Overworld {
   }
 
   init() {
-    const image = new Image();
-    
-    image.src = '/images/maps/DemoLower.png';
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0);
+    // Render Map
+    const map = new Image();
+    map.onload = () => {
+      this.ctx.drawImage(map, 0, 0);
     };
+    map.src = '/images/maps/DemoLower.png';
+
+    let x = 5; 
+    let y = 6;
+
+    // Draw shadow
+    const shadow = new Image();
+    shadow.onload = () => {
+      this.ctx.drawImage(
+        shadow, 
+        0,  // left cut
+        0,  // top cut
+        32, // cut width
+        32, // cut height
+        x * 16 - 8, 
+        y * 16 - 18,
+        32,
+        32
+      );
+    };
+    shadow.src = '/images/characters/shadow.png';
+
+    // Draw hero
+    const hero = new Image();
+    hero.onload = () => {
+      this.ctx.drawImage(
+        hero, 
+        0,  // left cut
+        0,  // top cut
+        32, // cut width
+        32, // cut height
+        x * 16 - 8, 
+        y * 16 - 18,
+        32,
+        32
+      );
+    }
+    hero.src = '/images/characters/people/hero.png';
   }
 
 
